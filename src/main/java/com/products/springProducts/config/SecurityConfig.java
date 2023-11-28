db9e73bbe693ca1abdba7f63c1533ebd9b45e303
Gocan Sergiu <gocan.sergiu97@gmail.com>
2023-11-28 19:17:49 +0200

    Added basic auth to the application

--- src/main/java/com/products/springProducts/config/SecurityConfig.java
+++ src/main/java/com/products/springProducts/config/SecurityConfig.java
@@ -2,32 +2,47 @@ package com.products.springProducts.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public UserDetailsService user() {
+    public InMemoryUserDetailsManager userDetailsService()  {
 
 
         UserDetails user = User.withDefaultPasswordEncoder()
                 .username("user")
                 .password("user")
-                .roles()
+                .roles("USER")
                 .build();
 
         UserDetails admin = User.withDefaultPasswordEncoder()
                 .username("admin")
                 .password("admin")
-                .roles("USER", "ADMIN")
+                .roles("ADMIN")
                 .build();
 
         return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/product/admin/**").hasRole("ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        return httpSecurity.build();
     }
 }

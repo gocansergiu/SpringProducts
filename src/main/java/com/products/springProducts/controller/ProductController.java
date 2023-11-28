df423370ddcab5eeb0d8a5ca42531f381d178626
Gocan Sergiu <gocan.sergiu97@gmail.com>
2023-11-28 21:28:18 +0200

    Added logs and repaired sonarQube inspection

--- src/main/java/com/products/springProducts/controller/ProductController.java
+++ src/main/java/com/products/springProducts/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@SuppressWarnings("unused")
 @RestController
 @RequestMapping("product")
 public class ProductController {
